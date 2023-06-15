0c2d9b2f46547676005b26dd763b04a9b71065da
KASHWAY <targosz.hubert@gmail.com>
2023-06-15 18:09:30 +0200

    Some fixes

--- partials/addUser.php
+++ partials/addUser.php
@@ -1,41 +1,48 @@
 <?php 
     $json_file = 'users.json';
-    $json_data = file_get_contents($json_file);
-    $data = json_decode($json_data, true);
 
-    $newUser = array(
-        'id' => count($data) + 1,
-        'name' => $_POST['name'],
-        'username' => $_POST['username'],
-        'email' => $_POST['email'],
-        'address' => array( 
-            'street' => $_POST['street'],
-            'suite' => $_POST['suite'],
-            'city' => $_POST['city'],
-            'zipcode' => $_POST['zipcode'],
-        ),
-        'phone' => $_POST['phone'],
-        'website' => $_POST['website'],
-        'company' => array(
-            'name' => $_POST['company'],
-        )
-        
-    );
+    if ($json_file != null) {
+        $json_data = file_get_contents($json_file);
+        $data = json_decode($json_data, true);
 
-    $data[] = $newUser;
+        $newUser = array(
+            'id' => count($data) + 1,
+            'name' => $_POST['name'],
+            'username' => $_POST['username'],
+            'email' => $_POST['email'],
+            'address' => array( 
+                'street' => $_POST['street'],
+                'suite' => $_POST['suite'],
+                'city' => $_POST['city'],
+                'zipcode' => $_POST['zipcode'],
+            ),
+            'phone' => $_POST['phone'],
+            'website' => $_POST['website'],
+            'company' => array(
+                'name' => $_POST['company'],
+            )
+            
+        );
+    
+        $data[] = $newUser;
+    
+    
+        $updated_data = json_encode($data, JSON_PRETTY_PRINT);
+    
+        if (file_put_contents($json_file, $updated_data)) {
+            sleep(1);
+            header("Location: http://localhost/backend-recruitment-task/");
+            exit;
+        }
 
-
-    $updated_data = json_encode($data, JSON_PRETTY_PRINT);
-
-    if (file_put_contents($json_file, $updated_data)) {
-        sleep(1);
-        header("Location: http://localhost/backend-recruitment-task/");
-        exit;
-        ?>
-<?php 
+        else {
+            echo 'Failed to add a user';
+        }
+    
     }
+   
     else {
-        echo 'Failed to add a user';
+        echo 'Could not reach the file.';
     }
 ?>
 
--- partials/main.php
+++ partials/main.php
@@ -87,7 +87,6 @@
                 xhttp.onreadystatechange = function() {
                     if (this.readyState === 4 && this.status === 200) {
                         location.reload();
-                        document.getElementById("demo") = this.responseText;
                     }
                 };
 
